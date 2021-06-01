05b62fa60b147738aa7112b23c05b48d928a2291
堺光之介 <20vr007h@rikkyo.ac.jp>
2021-06-01 20:41:40 +0900

    サンプルコードに戻した

--- lib/ui/component/body/top_main_body.dart
+++ lib/ui/component/body/top_main_body.dart
@@ -1,13 +1,1 @@
-import 'package:flutter/material.dart';
 
-class TopMainBody extends StatefulWidget {
-  @override
-  _TopMainBody createState() => _TopMainBody();
-}
-
-class _TopMainBody extends State<TopMainBody> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(body: Text('dd'));
-  }
-}
