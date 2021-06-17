da4385772e61c0aaa4b2758d20ef0d002fd98f49
koh41680716 <20vr007h@rikkyo.ac.jp>
2021-06-17 16:36:45 +0900

    構成を整えた

---
+++ .zshrc
@@ -0,0 +1,1 @@
+export PATH="$PATH:/Users/sakaikounosuke/workspace/flutter/flutter/bin"
--- lib/ui/screen/top/top_main_screen.dart
+++ lib/ui/screen/top/top_main_screen.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/material.dart';
 
+class Top_mainScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('sdcs')),
+        body: Center(
+          child: RaisedButton(
+            onPressed: () {},
+          ),
+        ));
+  }
+}
